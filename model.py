a7bd2324918dc59ea3b78b5c17e1d9900384d841
Praise <mgc555@york.ac.uk>
2026-05-13 12:04:22 +0100

    Improve convolution blocks with batch normalisation

--- model.py
+++ model.py
@@ -2,35 +2,41 @@ import torch
 import torch.nn as nn
 
 
+def conv_block(in_c, out_c):
+    return nn.Sequential(
+        nn.Conv2d(in_c, out_c, 3, padding=1),
+        nn.BatchNorm2d(out_c),
+        nn.ReLU(),
+        nn.Conv2d(out_c, out_c, 3, padding=1),
+        nn.BatchNorm2d(out_c),
+        nn.ReLU(),
+        nn.MaxPool2d(2)
+    )
+
+
 class PetClassifier(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-        )
-
+        # convolution blocks
+        self.block1 = conv_block(3, 64)
+        self.block2 = conv_block(64, 128)
+        self.block3 = conv_block(128, 256)
         self.pool = nn.AdaptiveAvgPool2d((2, 2))
 
+        # classifier
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(256 * 2 * 2, 256),
+            nn.Linear(256 * 2 * 2, 512),
             nn.ReLU(),
-            nn.Linear(256, 37)
+            nn.Linear(512, 37)
         )
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
         x = self.pool(x)
         x = self.classifier(x)
+
         return x
